e8cc44ff27f7f20f2120a3c14fb6b17a45759456
swachitha <swachitha25@gmail.com>
2025-10-25 05:31:58 -0500

    Updated homepage message

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "<h1>Welcome to My DevOps Journey 🚀</h1>"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
